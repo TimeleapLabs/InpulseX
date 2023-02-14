f1eaa0d152693a297bf0b5e2ca07e8a9fb314dd5
Pouya <pouya.eghbali@yandex.com>
2023-02-14 19:59:20 +0100

    Add pending swap view

---
+++ UI/src/lib/components/swap/PastClaim.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Button from '../Button.svelte';
+	import Table from '../Table.svelte';
+	import Card from '../Card.svelte';
+	import { chainIds } from '../../swap';
+
+	import { ethers } from 'ethers';
+
+	export let swap;
+
+	const data = [
+		{ value: chainIds[swap.fromChain].title, value: 'From Chain' },
+		{ value: chainIds[swap.toChain].title, value: 'To Chain' },
+		{ value: ethers.utils.formatUnits(swap.amount), value: 'Amount' }
+	];
+</script>
+
+<Card>
+	<div class="past-claim">
+		<Table {data} />
+		<Button>Claim</Button>
+	</div>
+</Card>
