cc7a001d8755d57ab2c76c99c33bfc56e2b0c026
Pouya <pouya.eghbali@yandex.com>
2023-04-19 14:03:45 +0200

    Animate team

---
+++ UI/src/lib/components/Member.svelte
@@ -0,0 +1,131 @@
+<script>
+	import Title from './Title.svelte';
+	import { fade } from 'svelte/transition';
+
+	export let member;
+	export let onSelect;
+	export let index;
+
+	let frame = 0;
+
+	const animate = (e) => {
+		e.preventDefault();
+		onSelect(index);
+		if (frame === 0) {
+			frame = 1;
+			setTimeout(() => (frame = 2), 1000);
+		}
+	};
+</script>
+
+<a class="wrap" href="#animate" on:click={animate}>
+	<div class="profile">
+		{#if frame === 0}
+			<img src="/images/members/{member.image}.jpg" alt={member.name} />
+		{:else if frame > 0}
+			<video src="/videos/x.mp4" mute autoplay />
+		{/if}
+		{#if frame === 2}
+			<img
+				src="/images/members/{member.image}.suite.jpg"
+				alt={member.name}
+				class="overlay"
+				transition:fade={{ duration: 3000 }}
+			/>
+		{/if}
+		<div class="name">
+			<Title as="h4">{member.name}</Title>
+		</div>
+	</div>
+	<div class="dots">
+		{#each Array(9).fill() as _}
+			<div class="dot" />
+		{/each}
+	</div>
+	<div class="role">
+		<img src="/images/members/{member.image}.jpg" alt={member.name} />
+		<div class="text">
+			<Title as="h5">{member.role}</Title>
+		</div>
+	</div>
+</a>
+
+<style>
+	a {
+		text-align: center;
+		text-decoration: none;
+		color: #fff;
+		display: flex;
+		gap: 1em;
+		flex-direction: column;
+		align-items: center;
+	}
+	.profile {
+		width: 200px;
+		height: 200px;
+		overflow: hidden;
+		border-radius: 100px;
+		box-shadow: 0px 0px 8px 4px var(--shadow-color);
+		border: 2px solid var(--main-color);
+		position: relative;
+	}
+	.profile .name {
+		position: absolute;
+		z-index: 1;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		opacity: 0.9;
+	}
+	.profile .name :global(h4) {
+		font-size: 4em;
+	}
+	video,
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	.overlay {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	.role {
+		position: relative;
+		border: 2px solid var(--main-color);
+		border-radius: 8px;
+		min-width: 80%;
+		overflow: hidden;
+	}
+	.role img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		position: absolute;
+	}
+	.role .text {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		backdrop-filter: blur(32px);
+		padding: 0 1em;
+		box-sizing: border-box;
+	}
+	.role .text :global(h5) {
+		margin: 0.5em 0;
+	}
+	.dots {
+		display: flex;
+		gap: 0.5rem;
+	}
+	.dot {
+		width: 8px;
+		height: 8px;
+		border-radius: 50%;
+		background: var(--main-color);
+	}
+</style>
--- UI/src/lib/components/home/Team.svelte
+++ UI/src/lib/components/home/Team.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Title from '../Title.svelte';
 	import Paragraph from '../Paragraph.svelte';
+	import Member from '../Member.svelte';
 	import { fade } from 'svelte/transition';
 
 	const members = [
@@ -8,66 +9,66 @@
 			name: 'JAY',
 			role: 'Founder & CEO',
 			color: '#DC15A4',
-			image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg',
+			image: 'rod',
 			bio: [
 				'Jay, the founder and CEO, is a serial entrepreneur with 18 years of experience in marketing, technology and international business management. He co-founded an app company in 2013 in London, where he worked for more than three years. He also founded a venture to build the first immersive dome Park in London with partners in USA and Russia; the business, unfortunately, suffered an unexpected impact due to the Pandemic putting on hold the whole entertainment industry across the globe. Since 2017, Jay has been running a multimillion-dollar crypto fund, specialising in early-stage projects.',
 				'His passion for crypto, art, technology and ultimately the mysteries of the universe was the catalyst that inspired him to found InpulseX.'
 			]
-		},
-		{
-			name: 'ROD',
-			role: 'Operations Director',
-			color: '#FFFFFF',
-			image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg',
-			bio: ['Lorem ipsum', '']
-		},
-		{
-			name: 'RAFAEL',
-			role: 'Graphic Design Manager',
-			color: '#2B7BF3',
-			image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg'
-		},
-		{
-			name: 'ROD',
-			role: 'Operations Director',
-			color: '#FFFFFF',
-			image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg'
-		},
-		{
-			name: 'JAY',
-			role: 'Founder & CEO',
-			color: '#DC15A4',
-			image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg',
-			bio: [
-				'Jay, the founder and CEO, is a serial entrepreneur with 18 years of experience in marketing, technology and international business management. He co-founded an app company in 2013 in London, where he worked for more than three years. He also founded a venture to build the first immersive dome Park in London with partners in USA and Russia; the business, unfortunately, suffered an unexpected impact due to the Pandemic putting on hold the whole entertainment industry across the globe. Since 2017, Jay has been running a multimillion-dollar crypto fund, specialising in early-stage projects.',
-				'His passion for crypto, art, technology and ultimately the mysteries of the universe was the catalyst that inspired him to found InpulseX.'
-			]
-		},
-		{
-			name: 'ROD',
-			role: 'Operations Director',
-			color: '#FFFFFF',
-			image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg',
-			bio: ['Lorem ipsum', '']
-		},
-		{
-			name: 'RAFAEL',
-			role: 'Graphic Design Manager',
-			color: '#2B7BF3',
-			image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg'
-		},
-		{
-			name: 'ROD',
-			role: 'Operations Director',
-			color: '#FFFFFF',
-			image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg'
 		}
+		// {
+		// 	name: 'ROD',
+		// 	role: 'Operations Director',
+		// 	color: '#FFFFFF',
+		// 	image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg',
+		// 	bio: ['Lorem ipsum', '']
+		// },
+		// {
+		// 	name: 'RAFAEL',
+		// 	role: 'Graphic Design Manager',
+		// 	color: '#2B7BF3',
+		// 	image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg'
+		// },
+		// {
+		// 	name: 'ROD',
+		// 	role: 'Operations Director',
+		// 	color: '#FFFFFF',
+		// 	image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg'
+		// },
+		// {
+		// 	name: 'JAY',
+		// 	role: 'Founder & CEO',
+		// 	color: '#DC15A4',
+		// 	image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg',
+		// 	bio: [
+		// 		'Jay, the founder and CEO, is a serial entrepreneur with 18 years of experience in marketing, technology and international business management. He co-founded an app company in 2013 in London, where he worked for more than three years. He also founded a venture to build the first immersive dome Park in London with partners in USA and Russia; the business, unfortunately, suffered an unexpected impact due to the Pandemic putting on hold the whole entertainment industry across the globe. Since 2017, Jay has been running a multimillion-dollar crypto fund, specialising in early-stage projects.',
+		// 		'His passion for crypto, art, technology and ultimately the mysteries of the universe was the catalyst that inspired him to found InpulseX.'
+		// 	]
+		// },
+		// {
+		// 	name: 'ROD',
+		// 	role: 'Operations Director',
+		// 	color: '#FFFFFF',
+		// 	image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg',
+		// 	bio: ['Lorem ipsum', '']
+		// },
+		// {
+		// 	name: 'RAFAEL',
+		// 	role: 'Graphic Design Manager',
+		// 	color: '#2B7BF3',
+		// 	image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg'
+		// },
+		// {
+		// 	name: 'ROD',
+		// 	role: 'Operations Director',
+		// 	color: '#FFFFFF',
+		// 	image: 'https://inpulsex.io/wp-content/uploads/2022/01/Rod.jpg'
+		// }
 	];
 
-	let currentIndex = 0;
+	let currentIndex;
 	let transition = false;
 
-	const onMouseEnter = (index) => (_e) => {
+	const onSelect = (index) => {
 		if (currentIndex != parseInt(index)) {
 			transition = true;
 			setTimeout(() => {
@@ -81,7 +82,7 @@
 <div class="team">
 	<div class="content">
 		<img class="title" src="/images/team.svg" alt="The InpulseX Team" />
-		{#if !transition}
+		{#if typeof currentIndex === 'number' && !transition}
 			<div class="bio" transition:fade>
 				<Title as="h4">{members[currentIndex].name}</Title>
 				<Title as="h5">{members[currentIndex].role}</Title>
@@ -93,28 +94,8 @@
 	</div>
 	<div class="grid">
 		{#each Object.entries(members) as [index, member]}
-			<div
-				on:mouseenter={onMouseEnter(index)}
-				class="member"
-				style="--main-color: {member.color}; --shadow-color: {member.color}66;"
-			>
-				<div class="profile">
-					<img src={member.image} alt={member.name} />
-					<div class="name">
-						<Title as="h4">{member.name}</Title>
-					</div>
-				</div>
-				<div class="dots">
-					{#each Array(9).fill() as _}
-						<div class="dot" />
-					{/each}
-				</div>
-				<div class="role">
-					<img src={member.image} alt={member.name} />
-					<div class="text">
-						<Title as="h5">{member.role}</Title>
-					</div>
-				</div>
+			<div class="member" style="--main-color: {member.color}; --shadow-color: {member.color}66;">
+				<Member {member} {onSelect} {index} />
 			</div>
 		{/each}
 	</div>
@@ -143,67 +124,6 @@
 		flex-direction: column;
 		align-items: center;
 		gap: 1em;
-	}
-	.member .profile {
-		width: 200px;
-		height: 200px;
-		overflow: hidden;
-		border-radius: 100px;
-		box-shadow: 0px 0px 8px 4px var(--shadow-color);
-		border: 2px solid var(--main-color);
-		position: relative;
-	}
-	.profile .name {
-		position: absolute;
-		z-index: 1;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		opacity: 0.9;
-	}
-	.profile .name :global(h4) {
-		font-size: 4em;
-	}
-	.member img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
-	.role {
-		position: relative;
-		border: 2px solid var(--main-color);
-		border-radius: 8px;
-		min-width: 80%;
-		overflow: hidden;
-	}
-	.role img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		position: absolute;
-	}
-	.role .text {
-		width: 100%;
-		height: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		backdrop-filter: blur(32px);
-		padding: 0 1em;
-		box-sizing: border-box;
-	}
-	.role .text :global(h5) {
-		margin: 0.5em 0;
-	}
-	.dots {
-		display: flex;
-		gap: 0.5rem;
-	}
-	.dot {
-		width: 8px;
-		height: 8px;
-		border-radius: 50%;
-		background: var(--main-color);
 	}
 	.bio {
 		margin-top: 2em;
